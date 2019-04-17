50c375e4c6cb18f774a2ed47bcb1ac1bd25e50c6
mrguup <dm@westmaglabs.com>
2019-04-16 21:02:44 -0500

    fixing switches

--- bot.js
+++ bot.js
@@ -131,33 +131,35 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 // Just add any case commands if you want to..
             }
 	} else {
-            case 'update':
-                if (conf.admins.includes(user)) {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: "Downloading updates and restarting!"
-                    });
-                    logger.info("Running git pull")
-                    git
-                        .pull()
-                        .then(function() {
-                            logger.info("GOING DOWN!")
-                            let stdout = execSync('/usr/local/bin/forever restart hydratron');
+            switch (cmd) {
+                case 'update':
+                    if (conf.admins.includes(user)) {
+                        bot.sendMessage({
+                            to: channelID,
+                            message: "Downloading updates and restarting!"
                         });
-                } else {
-                    logger.info(`${user} is trying to get fancy with the bot`);
+                        logger.info("Running git pull")
+                        git
+                            .pull()
+                            .then(function() {
+                                logger.info("GOING DOWN!")
+                                let stdout = execSync('/usr/local/bin/forever restart hydratron');
+                            });
+                    } else {
+                        logger.info(`${user} is trying to get fancy with the bot`);
+                        bot.sendMessage({
+                            to: channelID,
+                            message: "You aren't my real dad!"
+                        });
+                    }
+                break;
+                case 'whisper':
                     bot.sendMessage({
-                        to: channelID,
-                        message: "You aren't my real dad!"
-                    });
-                }
-            break;
-            case 'whisper':
-                bot.sendMessage({
-                    to: userID,
-                    message: `I heard "${args}"`
-                })
-            break;
+                        to: userID,
+                        message: `I heard "${args}"`
+                    })
+                break;
+            }
             //logger.info(`user ${user} tried to use the bot wrongly`)
             //bot.sendMessage({
             //    to: userID,
